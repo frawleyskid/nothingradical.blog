71be41b24bfb1944b1210abcf1879bd28e43b87f
Frawley <frawleyskid@gmail.com>
2022-05-27 00:49:43 -0400

    Simplify comments in CSP

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+window.hyvorCommentConfig = {
+  url: {{ .Page.Permalink | jsonify }},
+  id: {{ .Params.id | jsonify }},
+  title: {{ .Params.title | jsonify }},
+  authorEmail: {{ .Params.author.email | jsonify }},
+};
