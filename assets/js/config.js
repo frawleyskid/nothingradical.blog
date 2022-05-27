cf21698bbb66584d9273be82b2a7de611e5dd3c7
Frawley <frawleyskid@gmail.com>
2022-05-27 09:22:49 -0400

    Add dark mode for Hyvor comments

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,3 +4,29 @@ const hyvorCommentConfig = {
   title: {{ .Params.title | jsonify }},
   authorEmail: {{ .Params.author.email | jsonify }},
 };
+
+const hyvorCommentLightPalette = {
+  accent: "#000000",
+  accentText: "#ffffff",
+  footerHeader: "#fafafa",
+  footerHeaderText: "#484848",
+  box: "#ffffff",
+  boxText: "#111111",
+  boxLightText: "#aaaaaa",
+  backgroundText: "#111111"
+};
+
+const hyvorCommentDarkPalette = {
+  accent: "#ffffff",
+  accentText: "#000000",
+  footerHeader: "#424242",
+  footerHeaderText: "#cac7c7",
+  box: "#606060",
+  boxText: "#ffffff",
+  boxLightText: "#aaaaaa",
+  backgroundText: "#ffffff",
+};
+
+window.addEventListener("onColorSchemeChange", (event) => {
+  hyvor_talk.setPalette(event.detail === "light" ? hyvorCommentLightPalette : hyvorCommentDarkPalette);
+});
